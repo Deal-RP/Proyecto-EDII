51713e16ef3ed6037c47a284e7331d303e635320
Deal-RP <derly.rodas@gmail.com>
2020-04-07 15:56:42 -0600

    Arbol B* y SDES

---
+++ Proyecto_EDII/Controllers/WareHouseController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Proyecto_EDII.Models;
+using Arboles;
+
+namespace Proyecto_EDII.Controllers
+{
+    delegate string ObjectToString(object o);
+    delegate object StringToObject(string s);
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WareHouseController : ControllerBase
+    {
+        #region ADD METHODS
+        [HttpPost, Route("ADD1")]
+        public void AddOffice([FromForm]OfficeData info)
+        {
+            DatosArboles.Instance.key = 15;
+            ArbolB<OfficeData>.IniciarArbol("Office", new StringToObject(OfficeData.StringToOffice), new ObjectToString(OfficeData.ObjectToString));
+            for (int i = 0; i < 500; i++)
+            {
+                ArbolB<OfficeData>.InsertarArbol(new OfficeData { ID = i});
+            }
+        }
+
+        [HttpPost, Route("ADD2")]
+        public void AddProduct([FromForm]ProductData info)
+        {
+            DatosArboles.Instance.key = 15;
+            ArbolB<ProductData>.IniciarArbol("Product", new StringToObject(ProductData.StringToProducto), new ObjectToString(ProductData.ObjectToString));
+            for (int i = 0; i < 500; i++)
+            {
+                ArbolB<ProductData>.InsertarArbol(new ProductData { ID = i});
+            }
+            
+        }
+
+        [HttpPost, Route("ADD3")]
+        public void AddProductOffice([FromForm]OfficeProduct info)
+        {
+            DatosArboles.Instance.key = 15;
+            ArbolB<OfficeProduct>.IniciarArbol("ProductOffice", new StringToObject(OfficeProduct.StringToOffice), new ObjectToString(OfficeProduct.ObjectToString));
+            for (int i = 0; i < 500; i++)
+            {
+                ArbolB<OfficeProduct>.InsertarArbol(new OfficeProduct { ID = i});
+            }
+        }
+        #endregion
+    }
+}
---
+++ Proyecto_EDII/Models/TypeData.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Arboles;
+
+namespace Proyecto_EDII.Models
+{
+    #region PRODUCT
+    public class ProductData : IComparable
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return this.ID.CompareTo(((ProductData)obj).ID);
+        }
+
+        public static string ObjectToString(object Nuevo)
+        {
+            var Actual = (ProductData)Nuevo;
+            Actual.Name = Actual.Name == null ? string.Empty : Actual.Name;
+            Actual.Address = Actual.Address == null ? string.Empty : Actual.Address;
+            var aux = $"{string.Format("{0,-100}", Actual.ID.ToString())}=/=/={string.Format("{0,-100}", Actual.Name)}=/=/={string.Format("{0,-100}", Actual.Address)}";
+
+            return SDES.CifradoDecifrado(DatosArboles.Instance.key, aux, true);
+        }
+
+        public static ProductData StringToProducto(string info)
+        {
+            var aux = SDES.CifradoDecifrado(DatosArboles.Instance.key, info, false);
+            var infoSeparada = aux.Split(new string[] { "=/=/=" }, StringSplitOptions.RemoveEmptyEntries);
+            return new ProductData() { ID = Convert.ToInt32(infoSeparada[0].Trim()), Name = infoSeparada[1].Trim(), Address = infoSeparada[2].Trim()};
+        }
+    }
+    #endregion
+
+    #region OFFICE
+    public class OfficeData : IComparable
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return this.ID.CompareTo(((OfficeData)obj).ID);
+        }
+
+        public static string ObjectToString(object Nuevo)
+        {
+            var Actual = (OfficeData)Nuevo;
+            Actual.Name = Actual.Name == null ? string.Empty : Actual.Name;
+            var aux = $"{string.Format("{0,-100}", Actual.ID.ToString())}=/=/={string.Format("{0,-100}", Actual.Name)}=/=/={string.Format("{0,-100}", Actual.Price.ToString())}";
+            
+            return SDES.CifradoDecifrado(DatosArboles.Instance.key, aux, true);
+        }
+
+        public static OfficeData StringToOffice(string info)
+        {
+            var aux = SDES.CifradoDecifrado(DatosArboles.Instance.key, info, false);
+            var infoSeparada = aux.Split(new string[] { "=/=/=" }, StringSplitOptions.RemoveEmptyEntries);
+            return new OfficeData() { ID = Convert.ToInt32(infoSeparada[0].Trim()), Name = infoSeparada[1].Trim(), Price = Convert.ToDouble(infoSeparada[2].Trim()) };
+        }
+    }
+    #endregion
+
+    #region OFFICE-PRODCUT
+    public class OfficeProduct : IComparable
+    {
+        public int ID { get; set; }
+        public int IdProduct { get; set; }
+        public int Inventory { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return this.ID.CompareTo(((OfficeProduct)obj).ID);
+        }
+
+        public static string ObjectToString(object Nuevo)
+        {
+            var Actual = (OfficeProduct)Nuevo;
+            var aux = $"{string.Format("{0,-100}", Actual.ID.ToString())}=/=/={string.Format("{0,-100}", Actual.IdProduct.ToString())}=/=/={string.Format("{0,-100}", Actual.Inventory.ToString())}";
+            
+            return SDES.CifradoDecifrado(DatosArboles.Instance.key, aux, true);
+        }
+
+        public static OfficeProduct StringToOffice(string info)
+        {
+            var aux = SDES.CifradoDecifrado(DatosArboles.Instance.key, info, false);
+            var infoSeparada = aux.Split(new string[] { "=/=/=" }, StringSplitOptions.RemoveEmptyEntries);
+            return new OfficeProduct() { ID = Convert.ToInt32(infoSeparada[0].Trim()), IdProduct = Convert.ToInt32(infoSeparada[1].Trim()), Inventory = Convert.ToInt32(infoSeparada[2].Trim()) };
+        }
+    }
+    #endregion
+}
